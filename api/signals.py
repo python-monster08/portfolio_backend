cf0a8b86e26a9a67d7258935a59217e4b0178953
Kamlesh Lovewanshi <85796224+python-monster08@users.noreply.github.com>
2025-05-08 12:51:19 +0530

    Update signals.py

--- api/signals.py
+++ api/signals.py
@@ -14,7 +14,36 @@ def create_superuser(sender, **kwargs):
 
         if not User.objects.filter(username=username).exists():
             print("Creating superuser...")
-            User.objects.create_superuser(username=username, email=email, password=password)
+            superuser = User.objects.create_superuser(
+                username=username,
+                email=email,
+                password=password
+            )
+            superuser.is_active = True
+            superuser.save()
             print(f"Superuser created: {username}")
         else:
             print("Superuser already exists.")
+
+
+
+# from django.contrib.auth import get_user_model
+# from django.db.models.signals import post_migrate
+# from django.dispatch import receiver
+# import os
+
+# @receiver(post_migrate)
+# def create_superuser(sender, **kwargs):
+#     """Automatically creates a superuser after migrations."""
+#     if sender.name == "api":  # Ensures this runs only for this app
+#         User = get_user_model()
+#         username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
+#         email = os.getenv("DJANGO_SUPERUSER_EMAIL", "kamleshlovewanshi2000@gmail.com")
+#         password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "Kamlesh@2025")
+
+#         if not User.objects.filter(username=username).exists():
+#             print("Creating superuser...")
+#             User.objects.create_superuser(username=username, email=email, password=password)
+#             print(f"Superuser created: {username}")
+#         else:
+#             print("Superuser already exists.")
